c8cccfba31f2f95e90b27295f2e15e773ecb4e7f
Lacho_t <82452750+lacho22@users.noreply.github.com>
2021-10-05 14:15:57 -0500

    Hola mundo incompleto de java
    sinstaxis lenguaje java

---
+++ hola_mundo_java.java
@@ -0,0 +1,12 @@
+public class hola_mundo_java{
+	/** 
+	 * Aplicacion java
+	 * Title	:Hola mundo en java
+	 * Description	:Hola mundo en java desde vim en widows
+	 * Author	:Ing Horacio RV
+	 * Date		:20211004
+	 * Version	:1
+	 * Usage	: c:\>javac /path/hola_mundo_java.java
+	 *
+	 * Hola mundo en java */
+}	
